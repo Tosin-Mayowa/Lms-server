9f5e24e46fa76bf714363940fbbf7f1a001e5ffc
Tosin-Mayowa <toss800@gmail.com>
2025-06-19 09:24:01 +0100

    fix import error

--- api/index.js
+++ api/index.js
@@ -1,7 +1,4 @@
-// api/index.js
 const serverless = require('serverless-http');
-require('dotenv').config({ path: 'config.env' });
-
 const app = require('../index'); // your Express app
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
